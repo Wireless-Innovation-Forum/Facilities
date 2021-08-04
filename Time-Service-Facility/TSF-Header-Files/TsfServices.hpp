a1044e55d46b3ecc1b2575d5f742967ded9afee5
OlivierKirsch <olivier.kirsch@kereval.com>
2021-08-04 14:18:58 +0200

    Fix indentation errors in comments

--- Time-Service-Facility/TSF-Header-Files/TsfServices.hpp
+++ Time-Service-Facility/TSF-Header-Files/TsfServices.hpp
@@ -1,24 +1,24 @@
 /*-----------------------------------------------------------------------------*/
 /*    Distributed by Wireless Innovation Forum                                 */
-/*    Copyright 2021 SDS Project Authors. All Rights Reserved.				   */
-/*    Licensed under the Apache License, Version 2.0 (the "License");		   */
-/*    you may not use this file except in compliance with the License.		   */
+/*    Copyright 2021 SDS Project Authors. All Rights Reserved.                 */
+/*    Licensed under the Apache License, Version 2.0 (the "License");          */
+/*    you may not use this file except in compliance with the License.         */
 /*    You may obtain a copy of the License at                                  */
 /*                                                                             */
-/*        http://www.apache.org/licenses/LICENSE-2.0						   */
-/*																			   */
-/*    Unless required by applicable law or agreed to in writing, software	   */
-/*    distributed under the License is distributed on an "AS IS" BASIS,		   */
+/*        http://www.apache.org/licenses/LICENSE-2.0                           */
+/*                                                                             */
+/*    Unless required by applicable law or agreed to in writing, software      */
+/*    distributed under the License is distributed on an "AS IS" BASIS,        */
 /*    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. */
 /*    See the License for the specific language governing permissions and      */
-/*    limitations under the License.										   */
+/*    limitations under the License.                                           */
 /*-----------------------------------------------------------------------------*/
-/* Title : TsfServices.hpp					 		                           */
-/* Date:  2021-08-04														   */
-/* Version:  1.0.0															   */
+/* Title : TsfServices.hpp                                                     */
+/* Date:  2021-08-04                                                           */
+/* Version:  1.0.0                                                             */
 /* Purpose: The TsfServices.hpp file is specified as the header file that      */
 /*          declares the service interfaces of the Time Service Facility       */
-/*	 		PIM Specification. 											       */
+/*          PIM Specification.                                                 */
 /*-----------------------------------------------------------------------------*/
 
 #ifndef TSF_SERVICES
